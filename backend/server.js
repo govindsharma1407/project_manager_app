c23e9986d57a2e95308f170c6feb801b38356101
Govind Gopal Sharma <monu66133@gmail.com>
2026-05-01 20:02:19 +0530

    Add backend test routes

--- backend/server.js
+++ backend/server.js
@@ -20,7 +20,6 @@ app.get("/", (req, res) => {
 app.use("/api/auth", require("./routes/authRoutes"));
 app.use("/api/projects", require("./routes/projectRoutes"));
 app.use("/api/tasks", require("./routes/taskRoutes"));
-
 mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log("MongoDB Connected"))
   .catch(err => console.log("Mongo Error:", err));
