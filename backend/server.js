6996219ecdab8caa63b85feabc18190206ca4f16
Govind Gopal Sharma <monu66133@gmail.com>
2026-05-01 17:20:46 +0530

    Connect frontend to deployed backend

--- backend/server.js
+++ backend/server.js
@@ -17,4 +17,6 @@ app.use("/api/auth", require("./routes/authRoutes"));
 app.use("/api/projects", require("./routes/projectRoutes"));
 app.use("/api/tasks", require("./routes/taskRoutes"));
 
-app.listen(5000, () => console.log("Server running on 5000"));
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server running on ${PORT}`));
